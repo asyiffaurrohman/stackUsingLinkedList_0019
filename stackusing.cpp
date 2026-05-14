1bd390bf19eedfc1eb620e0811fb21754e1d2c83
asyiffaurrohman <akungithub934@gmail.com>
2026-05-14 15:31:59 +0700

    add default case in main function for stack menu

--- stackusing.cpp
+++ stackusing.cpp
@@ -116,4 +116,15 @@ int main()
             }
             break;
 
-        
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
+        }
+        cout << endl;
+    }
+    return 0;
+};
+      
