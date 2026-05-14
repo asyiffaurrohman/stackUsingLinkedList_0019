9734f2603eac832a403d7eee5cfd0141ae6d70f6
asyiffaurrohman <akungithub934@gmail.com>
2026-05-14 15:24:09 +0700

    fix peek method to correctly display stack elements

--- stackusing.cpp
+++ stackusing.cpp
@@ -54,4 +54,15 @@ public:
             cout << "List is empty." << endl;
         }
         else
-        
+        {
+            Node *current = top;
+             while (current != NULL)
+            {
+                cout << current->data << " ";
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+    
